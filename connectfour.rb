11f1db47d475eb151a458746f16e32e9cc8fbbff
Laura Hinojos <hinojoslj@gmail.com>
2018-05-01 18:21:22 -0600

    Vertical & Horizontal Block in Progress

--- connectfour.rb
+++ connectfour.rb
@@ -78,6 +78,10 @@ class Board
     (0..6).any? {|c| (0..2).any? {|r| threeFromTowards?(player, r, c, 1, 0)}}
   end
   
+  def horizontalBlock? (player)
+    (0..3).any? {|c| (0..5).any? {|r| threeFromTowards?(player, r, c, 0, 1)}}
+  end
+  
   def threeFromTowards?(player, r, c, dx, dy)
     return (0..2).all?{|step| @board[r+step*dx][c+step*dy] == player}
   end
@@ -86,9 +90,15 @@ class Board
 #------------------------------------------------------------------------------
   def blockCheck (player)
     if verticalBlock?(player)
-      puts("No Block Needed");
+      puts("#{player} - No Vertical Block Needed");
     else
-      puts("Perform a block");
+      puts("#{player} - Perform a Vertical block");
+    end
+    
+    if horizontalBlock?(player)
+      puts("#{player} - No Horizontal Block Needed");
+    else
+      puts("#{player} - Perform a Horizontal block");
     end
   end
 
@@ -120,6 +130,7 @@ end
 
 #------------------------------------------------------------------
 # test some robot-player behaviors
+puts("~~~~~~~~~~~~~~BLOCK 1~~~~~~~~~~~~~~~~~~~~~");
 testboard1 = Board.new
 testboard1.addDisc(:R,4)
 testboard1.addDisc(:O,4)
@@ -127,41 +138,61 @@ testboard1.addDisc(:R,5)
 testboard1.addDisc(:O,5)
 testboard1.addDisc(:R,6)
 testboard1.addDisc(:O,6)
-testResult(:block1, robotMove(:R, testboard1),[3], 'robot should block horiz')
+testResult(:BLOCK1, robotMove(:R, testboard1),[3], 'robot should block horiz')
 testboard1.print
 
+puts("~~~~~~~~~~~~~~BLOCK 2~~~~~~~~~~~~~~~~~~~~~");
 testboard2 = Board.new
 #testboard2.addDiscs(:R, [3, 1, 3, 2, 3]);
-testboard2.addDiscs(:R, [3, 1, 3, 2]);
-testResult(:block2, robotMove(:O, testboard2), [3], 'robot should block vert')
+testboard2.addDiscs(:R, [3, 1, 0, 2]);
+testResult(:BLOCK2, robotMove(:R, testboard2), [3], 'robot should block vert')
 #0|1|2|3|4|5|6
-# | | | | | |
-# | | | | | |
-# | | | | | |
-# | | |R| | |
-# | | |R| | |
-# |O|O|R| | |
+# | | | | | | 5
+# | | | | | | 4
+# | | | | | | 3
+# | | |R| | | 2
+# | | |R| | | 1
+# |O|O|R| | | 0
 testboard2.print
 
+puts("~~~~~~~~~~~~~~BLOCK 3~~~~~~~~~~~~~~~~~~~~~");
 testboard2 = Board.new
 testboard2.addDiscs(:O, [3, 1, 3, 2, 3, 4]);
-testResult(:block3, robotMove(:O, testboard2), [3], 'robot should complete stack')
+#0|1|2|3|4|5|6
+# | | | | | |  5
+# | | | | | |  4
+# | | | | | |  3
+# | | |O| | |  2
+# | | |O| | |  1
+# |R|R|O|R| |  0
+
+testResult(:BLOCK3, robotMove(:O, testboard2), [3], 'robot should complete stack')
 testboard2.print
 
+puts("~~~~~~~~~~~~~~BLOCK 4~~~~~~~~~~~~~~~~~~~~~");
 testboard2 = Board.new
 testboard2.addDiscs(:R, [3, 1, 4, 5, 2, 1, 6, 0, 3, 4, 5, 3, 2, 2, 6 ]);
 #0|1|2|3|4|5|6
-# | | | | | |
-# | | | | | |
-# | | | | | |
-# | |O|O| | |
-# |O|R|R|O|R|R
-#O|O|R|R|R|O|R
-testResult(:block4, robotMove(:O, testboard2), [3], 'robot should complete diag')
+# | | | | | |  5
+# | | | | | |  4
+# | | | | | |  3
+# | |O|O| | |  2
+# |O|R|R|O|R|R 1
+#O|O|R|R|R|O|R 0
+testResult(:BLOCK4, robotMove(:O, testboard2), [3], 'robot should complete diag')
 testboard2.print
 
+puts("~~~~~~~~~~~~~~AVOID~~~~~~~~~~~~~~~~~~~~~");
 testboard3 = Board.new
-testboard3.addDiscs(:O, [1,1,2,2,3,3])
+testboard3.addDiscs(:O, [1,1,2,2,3,3,4,4])
+#0|1|2|3|4|5|6
+# | | | | | |  5
+# | | | | | |  4
+# | | | | | |  3
+# | | | | | |  2
+# |R|R|R| | |  1
+# |O|O|O| | |  0
+
 testResult(:avoid, robotMove(:O, testboard3), [0,6], 'robot should avoid giving win')
 testboard3.print
 
